6ee1163f5ab4daeda80ff69414b7cc2ab19d71f3
rkouzmine <krrsea@gmail.com>
2025-08-23 00:14:41 +0300

    Определить время дня по часу

--- src/main/java/ru/job4j/condition/Task66.java
+++ src/main/java/ru/job4j/condition/Task66.java
@@ -2,14 +2,22 @@ package ru.job4j.condition;
 
 public class Task66 {
     public static void daytime(int number) {
-        String result = "Не является часом";
-        switch (number) {
-            case 6, 7, 8, 9, 10 -> result = "Утро";
-            case 11, 12, 13, 14, 15, 16, 17 -> result = "День";
-            case 18, 19, 20, 21, 22 -> result = "Вечер";
-            case 23, 0, 1, 2, 3, 4, 5 -> result = "Ночь";
-            default -> result = "Не является часом";
-        }
+        String result = switch (number) {
+            case 6, 7, 8, 9, 10 -> "Утро";
+            case 11, 12, 13, 14, 15, 16, 17 -> "День";
+            case 18, 19, 20, 21, 22 -> "Вечер";
+            case 23, 0, 1, 2, 3, 4, 5 -> "Ночь";
+            default -> "Не является часом";
+        };
         System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        daytime(10);
+        daytime(15);
+        daytime(20);
+        daytime(23);
+        daytime(0);
+        daytime(24);
     }
 }
